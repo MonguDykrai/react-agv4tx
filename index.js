6ffec306899d8b382561688d9bdae673d4ed8635
MonguDykrai <kisaihou@gmail.com>
2021-10-24 00:33:37 +0800

    class 组件改为 hook 组件

--- index.js
+++ index.js
@@ -1,92 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 import { Select, Input, Button } from 'antd';
-import { CheckOutlined, DeleteOutlined } from '@ant-design/icons';
-const { Option } = Select; // ok
+import { CheckOutlined,  } from '@ant-design/icons';
 
-let index = 0;
-
-class App extends React.Component {
-  state = {
-    items: [
-      { name: '设备预热', deletable: false, selected: true },
-      { name: '等料', deletable: true, selected: false },
-    ],
-    name: '',
-  };
-
-  onNameChange = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
-  };
-
-  addItem = () => {
-    console.log('addItem');
-    const { items, name } = this.state;
-    this.setState({
-      items: [...items, name || `New item ${index++}`],
-      name: '',
-    });
-  };
-
-  render() {
-    const { items, name } = this.state;
-    return (
-      <Select
-        open={true} // 打开 dropdown
-        defaultValue={this.state.items[1].name}
-        dropdownRender={() => {
-          return (
-            <div>
-              {items.map((item) => (
-                <div className="select-item-option select-item" key={item.name}>
-                  <span style={{ marginRight: 1 }}>
-                    <svg
-                      t="1635005909243"
-                      class="icon"
-                      viewBox="0 0 1024 1024"
-                      version="1.1"
-                      xmlns="http://www.w3.org/2000/svg"
-                      p-id="1215"
-                      width="24"
-                      height="24"
-                    >
-                      <path
-                        d="M649.824 604.576a31.968 31.968 0 1 1-45.248 45.248L505.6 550.848l-98.976 98.976a31.904 31.904 0 0 1-45.248 0 32 32 0 0 1 0-45.248l98.976-98.976-98.976-98.976a32 32 0 0 1 45.248-45.248l98.976 98.976 98.976-98.976a32 32 0 0 1 45.248 45.248L550.848 505.6l98.976 98.976zM512 128C300.288 128 128 300.288 128 512c0 211.744 172.288 384 384 384 211.744 0 384-172.256 384-384 0-211.712-172.256-384-384-384z"
-                        p-id="1216"
-                        fill="#6B6F8D"
-                      ></path>
-                    </svg>
-                  </span>
-                  {item.name}
-                  <CheckOutlined
-                    style={{ marginLeft: 'auto', color: '#3C7DFF' }}
-                  />
-                </div>
-              ))}
-              <div className="select-dropdown-input">
-                <Input
-                  placeholder="请输入"
-                  value={name}
-                  onChange={this.onNameChange}
+const App = () => {
+  const [items, setItems] = useState([
+    { name: '设备预热', deletable: false, selected: true },
+    { name: '等料', deletable: true, selected: false },
+  ]);
+  const [value, setValue] = useState('');
+  return (
+    <Select
+      open={true} // 打开 dropdown
+      defaultValue={items[1].name}
+      dropdownRender={() => {
+        return (
+          <div>
+            {items.map((item) => (
+              <div className="select-item-option select-item" key={item.name}>
+                <span style={{ marginRight: 1 }}>
+                  <svg
+                    t="1635005909243"
+                    class="icon"
+                    viewBox="0 0 1024 1024"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    p-id="1215"
+                    width="24"
+                    height="24"
+                  >
+                    <path
+                      d="M649.824 604.576a31.968 31.968 0 1 1-45.248 45.248L505.6 550.848l-98.976 98.976a31.904 31.904 0 0 1-45.248 0 32 32 0 0 1 0-45.248l98.976-98.976-98.976-98.976a32 32 0 0 1 45.248-45.248l98.976 98.976 98.976-98.976a32 32 0 0 1 45.248 45.248L550.848 505.6l98.976 98.976zM512 128C300.288 128 128 300.288 128 512c0 211.744 172.288 384 384 384 211.744 0 384-172.256 384-384 0-211.712-172.256-384-384-384z"
+                      p-id="1216"
+                      fill="#6B6F8D"
+                    ></path>
+                  </svg>
+                </span>
+                {item.name}
+                <CheckOutlined
+                  style={{ marginLeft: 'auto', color: '#3C7DFF' }}
                 />
               </div>
-              <div className="add-custom-tag">
-                <Button type="primary">自定义标签</Button>
-              </div>
-              <div className="action-button-group">
-                <Button type="ghost">取消</Button>
-                <Button type="primary">创建</Button>
-              </div>
+            ))}
+            <div className="select-dropdown-input">
+              <Input
+                placeholder="请输入"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
             </div>
-          );
-        }}
-      />
-    );
-  }
-}
+            <div className="add-custom-tag">
+              <Button type="primary">自定义标签</Button>
+            </div>
+            <div className="action-button-group">
+              <Button type="ghost">取消</Button>
+              <Button type="primary">创建</Button>
+            </div>
+          </div>
+        );
+      }}
+    />
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('container'));
