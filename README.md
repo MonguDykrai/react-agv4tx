5adbe6a490e02c2c70b7a4bc941bab5462a686ec
MonguDykrai <kisaihou@gmail.com>
2021-10-23 22:25:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-agv4tx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-agv4tx)
---
+++ index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Select, Input, Button } from 'antd';
+
+const { Option } = Select;
+
+let index = 0;
+
+class App extends React.Component {
+  state = {
+    items: ['设备预热', '等料'],
+    name: '',
+  };
+
+  onNameChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  addItem = () => {
+    console.log('addItem');
+    const { items, name } = this.state;
+    this.setState({
+      items: [...items, name || `New item ${index++}`],
+      name: '',
+    });
+  };
+
+  render() {
+    const { items, name } = this.state;
+    return (
+      <Select
+        style={{ width: 240 }}
+        placeholder="custom dropdown render"
+        dropdownRender={(menu) => (
+          <div>
+            {menu}
+            <div style={{ display: 'flex', flexWrap: 'nowrap', padding: 8 }}>
+              <Input
+                placeholder="请输入"
+                style={{
+                  flex: 'auto',
+                  background: '#23284D',
+                  borderRadius: 4,
+                  height: 40,
+                  width: 160,
+                  color: '#fff',
+                }}
+                value={name}
+                onChange={this.onNameChange}
+              />
+            </div>
+            <div>
+              <Button type="ghost">取消</Button>
+              <Button type="primary">创建</Button>
+            </div>
+          </div>
+        )}
+      >
+        {items.map((item) => (
+          <Option key={item}>{item}</Option>
+        ))}
+      </Select>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
